733e1daa5182a3bb994be6a517c693c0c9fc95ad
Sothink <rebenkov2002@mail.ru>
2022-10-17 07:35:09 +0300

    feat: add task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,21 +6,73 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+class TreeNode {
+  constructor(data) {
+    this.data = data;
+    this.right = null;
+    this.left = null;
+  } 
+}
+
+
 class BinarySearchTree {
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.root = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  roota() {
+    console.log(this.root.data)
+    return this.root.data;
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if(!this.root) {
+      this.root = new TreeNode(data)
+    } else {
+      let node = this.root;
+      let newNode = new TreeNode(data)
+      while (node) {
+        if(data > node.data) {
+          if (!node.right) {
+            break
+           }
+          node = node.right;
+        } else {
+          if (!node.left) {
+            break
+          }
+          node = node.left;
+        }
+      }
+      if (data > node.data) {
+        node.right = newNode;
+      } else {
+        node.left = newNode;
+      };
+    };
+  }
+
+  print(root = this.root) {
+    if (!root) {
+      return true;
+    };
+    console.log(root.data);
+    this.print(root.left);
+    this.print(root.right);
+  }
+
+  has(data) {
+    if(node === null) {
+      return null;
+    } else if (data < node.data) {
+      return this.has(node.left, data);
+    } else if(data > node.data) {
+      return this.has(node.right, data);
+    } else {
+      return node;
+    }
   }
 
   find(/* data */) {
@@ -47,3 +99,17 @@ class BinarySearchTree {
 module.exports = {
   BinarySearchTree
 };
+
+const tree = new BinarySearchTree();
+
+tree.add(3)
+tree.add(5)
+tree.add(1)
+tree.add(8)
+tree.add(10)
+
+tree.print()
+
+tree.roota()
+
+console.log(tree.has())
